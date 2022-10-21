ee6d51c224b68c19cd5abcc4f214eb47b93d442b
Razieh Abedi <raziyeabedi@gmail.com>
2022-10-21 17:47:49 +0800

    created the app

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from './pages/Home';
+import Error from './pages/Error';
+import SingleCocktail from './pages/SingleCocktail';
+import About from './pages/About';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div >
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={ <Home/>}/>
+        <Route path='about' element={ <About/>}/>
+        <Route path='cocktail/:id' element={ <SingleCocktail/>}/>
+        <Route path='*' element={ <Error/>}/>
+   
+    
+    
+    
+    </Routes>
+    </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
